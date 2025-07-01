55b1e24b343712b5cc59b82ec9c3252249907a15
Danie Antón <danielantcam@gmail.com>
2025-07-01 22:20:53 +0200

    Minor changes for deploy 2.

--- client/src/fetcher.js
+++ client/src/fetcher.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://simple-url-shortener.onrender.com";
 
 export const mutate = async (method, endpoint, body)=>{
     let fetchParams = {
