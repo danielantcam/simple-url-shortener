7f1c7e7c90cf31a31eab4b1e4230a7523d9b7767
Danie Antón <danielantcam@gmail.com>
2025-07-11 16:01:44 +0200

    Vercel analytics set up

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App.jsx';
+import { Analytics } from "@vercel/analytics/next"  // Vercel analytics
 
 const queryClient = new QueryClient();
 
@@ -11,5 +12,6 @@ createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
+    <Analytics />
   </StrictMode>,
 )
