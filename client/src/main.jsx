8bea88a1c335ef8c1d38b816067d5c952dbab56a
Danie Antón <danielantcam@gmail.com>
2025-08-11 12:01:11 +0200

    Changed landing page + removed vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App.jsx';
-import { Analytics } from "@vercel/analytics/react"  // Vercel analytics
 
 const queryClient = new QueryClient();
 
@@ -12,6 +11,5 @@ createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-    <Analytics />
   </StrictMode>,
 )
